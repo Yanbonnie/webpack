5a3b92e7967d9dd4b7ad1be6591c9b0d8523a269
Bonnie <user_reg@qq.com>
2018-07-30 16:23:26 +0800

    webpack打包路径

---
+++ webpack_path/src/app.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './common/style/index.scss';
+
+import img from './common/img/giphy.gif';
+console.log(img);
+
+// ReactDOM.render(
+//     <div className="ot">
+//         <img src={require('./common/img/dogs.jpg')} alt=""/>
+//         <i className="fa fa-rocket"></i>
+//     </div>,
+//     document.getElementById('root')
+// );
